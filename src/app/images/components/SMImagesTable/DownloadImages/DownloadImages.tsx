9feba4d1ea793a104bddf189502d7522578c55db
abuyukyi101198 <acb3@outlook.com>
2024-12-25 16:33:15 +0300

    feat(download-images): added API function for DownloadImages.tsx (only for Ubuntu)

--- src/app/images/components/SMImagesTable/DownloadImages/DownloadImages.tsx
+++ src/app/images/components/SMImagesTable/DownloadImages/DownloadImages.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
-import { ContentSection } from "@canonical/maas-react-components";
 import type { MultiSelectItem } from "@canonical/react-components";
-import {
-  MultiSelect,
-  ActionButton,
-  Button,
-  Form,
-} from "@canonical/react-components";
-import { Field, Formik } from "formik";
+import { MultiSelect, Form, Strip } from "@canonical/react-components";
+import { Field } from "formik";
 import { useDispatch, useSelector } from "react-redux";
 
+import FormikForm from "@/app/base/components/FormikForm";
 import { useSidePanel } from "@/app/base/side-panel-context";
 import { bootResourceActions } from "@/app/store/bootresource";
 import bootResourceSelectors from "@/app/store/bootresource/selectors";
 import type {
+  BootResource,
   BootResourceUbuntuArch,
   BootResourceUbuntuRelease,
+} from "@/app/store/bootresource/types";
+import {
+  BootResourceSourceType,
+  BootResourceAction,
 } from "@/app/store/bootresource/types";
 
 import "./_index.scss";
@@ -29,13 +29,14 @@ type ReleasesWithArches = {
   [key: string]: MultiSelectItem[];
 };
 
-type ImagesByName = { [key: string]: DownloadableImage[] };
+type ImagesByOS = { [key: string]: DownloadableImage[] };
 
 type DownloadableImage = {
   id: string;
   name: string;
   release: string;
   architectures: string;
+  os: string;
 };
 
 const getDownloadableImages = (
@@ -46,37 +47,66 @@ const getDownloadableImages = (
     .map((image) => {
       return architectures.map((arch) => {
         return {
-          id: `ubuntu-${image.title}-${arch.name}`,
-          name: "Ubuntu",
+          id: `ubuntu-${image.name}-${image.title}-${arch.name}`,
+          name: image.name,
           release: image.title,
           architectures: arch.name,
+          os: "Ubuntu",
         };
       });
     })
     .flat();
 };
 
-const groupImagesByName = (images: DownloadableImage[]) => {
-  let imagesByName: ImagesByName = {};
+const getSyncedImages = (
+  downloadableImages: DownloadableImage[],
+  resources: BootResource[]
+): Record<string, { label: string; value: string }[]> => {
+  return downloadableImages
+    .filter((image) => {
+      return resources.some(
+        (resource) =>
+          resource.title === image.release &&
+          resource.arch === image.architectures
+      );
+    })
+    .reduce<Record<string, { label: string; value: string }[]>>(
+      (acc, image) => {
+        const key = `${image.os}-${image.release.replace(".", "-")}`;
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        acc[key].push({
+          label: image.architectures,
+          value: image.id,
+        });
+        return acc;
+      },
+      {}
+    );
+};
+
+const groupImagesByOS = (images: DownloadableImage[]) => {
+  let imagesByOS: ImagesByOS = {};
 
   images.forEach((image) => {
-    if (!!imagesByName[image.name]) {
-      imagesByName[image.name].push(image);
+    if (!!imagesByOS[image.os]) {
+      imagesByOS[image.os].push(image);
     } else {
-      imagesByName[image.name] = [image];
+      imagesByOS[image.os] = [image];
     }
   });
 
-  Object.keys(imagesByName).forEach((distro) => {
-    imagesByName[distro].sort((a, b) => {
+  Object.keys(imagesByOS).forEach((distro) => {
+    imagesByOS[distro].sort((a, b) => {
       return b.release.localeCompare(a.release);
     });
   });
 
-  return imagesByName;
+  return imagesByOS;
 };
 
-const groupArchesByRelease = (images: ImagesByName) => {
+const groupArchesByRelease = (images: ImagesByOS) => {
   let groupedImages: GroupedImages = {};
 
   Object.keys(images).forEach((distro) => {
@@ -100,30 +130,28 @@ const groupArchesByRelease = (images: ImagesByName) => {
   return groupedImages;
 };
 
-const getInitialState = (images: ImagesByName) => {
-  let initialState: ReleasesWithArches = {};
-
-  Object.keys(images).forEach((distro) => {
-    images[distro].forEach((image) => {
-      if (!initialState[getValueKey(distro, image.release)]) {
-        initialState[getValueKey(distro, image.release)] = [];
-      }
-    });
-  });
-
-  return initialState;
-};
-
 const getValueKey = (distro: string, release: string) =>
   `${distro}-${release}`.replace(".", "-");
 
 const DownloadImages: React.FC = () => {
   const dispatch = useDispatch();
   const ubuntu = useSelector(bootResourceSelectors.ubuntu);
+  const resources = useSelector(bootResourceSelectors.ubuntuResources);
 
-  const [images, setImages] = useState<ImagesByName>({});
+  const sources = ubuntu?.sources || [];
   const [groupedImages, setGroupedImages] = useState<GroupedImages>({});
-  const [initialValues, setInitialValues] = useState<ReleasesWithArches>({});
+  const [syncedImages, setSyncedImages] = useState({});
+
+  const eventErrors = useSelector(bootResourceSelectors.eventErrors);
+  const error = eventErrors.find(
+    (error) =>
+      error.event === BootResourceAction.SAVE_UBUNTU ||
+      error.event === BootResourceAction.STOP_IMPORT
+  )?.error;
+  const cleanup = useCallback(() => bootResourceActions.cleanup(), []);
+
+  const mainSource = sources.length > 0 ? sources[0] : null;
+  const tooManySources = sources.length > 1;
 
   useEffect(() => {
     if (ubuntu) {
@@ -131,12 +159,12 @@ const DownloadImages: React.FC = () => {
         ubuntu.releases,
         ubuntu.arches
       );
-      const imagesByName = groupImagesByName(downloadableImages);
-      setImages(imagesByName);
-      setGroupedImages(groupArchesByRelease(imagesByName));
-      setInitialValues(getInitialState(imagesByName));
+      const syncedImages = getSyncedImages(downloadableImages, resources);
+      setSyncedImages(syncedImages);
+      const imagesByOS = groupImagesByOS(downloadableImages);
+      setGroupedImages(groupArchesByRelease(imagesByOS));
     }
-  }, [ubuntu]);
+  }, [ubuntu, resources]);
 
   useEffect(() => {
     return () => {
@@ -148,77 +176,89 @@ const DownloadImages: React.FC = () => {
 
   const resetForm = () => {
     setSidePanelContent(null);
-    setInitialValues(images ? getInitialState(images) : {});
   };
 
   return (
-    <ContentSection>
-      <>
-        <ContentSection.Content>
-          <Formik
-            enableReinitialize={true}
-            initialValues={initialValues}
-            onSubmit={(values) => console.log(values)}
-          >
-            {({ isSubmitting, dirty, values, setFieldValue }) => (
-              <Form>
-                {Object.keys(groupedImages).map((distro) => (
-                  <span key={distro}>
-                    <h2 className="p-heading--4">{distro} images</h2>
-                    <table className="download-images-table">
-                      <thead>
-                        <tr>
-                          <th>Release</th>
-                          <th>Architecture</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {Object.keys(groupedImages[distro]).map((release) => (
-                          <tr aria-label={release} key={release}>
-                            <td>{release}</td>
-                            <td>
-                              <Field
-                                as={MultiSelect}
-                                items={groupedImages[distro][release]}
-                                name={`${distro}-${release}`}
-                                onItemsUpdate={(items: MultiSelectItem) =>
-                                  setFieldValue(
-                                    getValueKey(distro, release),
-                                    items
-                                  )
-                                }
-                                placeholder="Select architectures"
-                                selectedItems={
-                                  values[getValueKey(distro, release)]
-                                }
-                                variant="condensed"
-                              />
-                            </td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
-                  </span>
-                ))}
-                <ContentSection.Footer>
-                  <Button appearance="base" onClick={resetForm} type="button">
-                    Cancel
-                  </Button>
-                  <ActionButton
-                    appearance="positive"
-                    disabled={!dirty || isSubmitting}
-                    loading={isSubmitting}
-                    type="submit"
-                  >
-                    Save
-                  </ActionButton>
-                </ContentSection.Footer>
-              </Form>
-            )}
-          </Formik>
-        </ContentSection.Content>
-      </>
-    </ContentSection>
+    <Strip shallow>
+      <FormikForm
+        allowUnchanged
+        buttonsBehavior="independent"
+        cleanup={cleanup}
+        editable={!tooManySources}
+        enableReinitialize
+        errors={error}
+        initialValues={syncedImages}
+        onCancel={resetForm}
+        onSubmit={(values) => {
+          dispatch(cleanup());
+          const osystems = Object.entries(
+            values as Record<string, { label: string; value: string }[]>
+          ).map(([key, images]) => {
+            const [osystem] = key.split("-", 1);
+            const arches = images.map((image) => image.label);
+            const release = images[0].value.split("-")[1];
+            return {
+              arches,
+              osystem: osystem.toLowerCase(),
+              release,
+            };
+          });
+          const params = mainSource
+            ? {
+                osystems,
+                ...mainSource,
+              }
+            : {
+                osystems,
+                source_type: BootResourceSourceType.MAAS_IO,
+              };
+          dispatch(bootResourceActions.saveUbuntu(params));
+          resetForm();
+        }}
+        onSuccess={() => {
+          dispatch(bootResourceActions.poll({ continuous: false }));
+        }}
+        submitLabel={"Download"}
+      >
+        {({ values, setFieldValue }: { values: any; setFieldValue: any }) => (
+          <Form>
+            {Object.keys(groupedImages).map((distro) => (
+              <span key={distro}>
+                <h2 className="p-heading--4">{distro} images</h2>
+                <table className="download-images-table">
+                  <thead>
+                    <tr>
+                      <th>Release</th>
+                      <th>Architecture</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {Object.keys(groupedImages[distro]).map((release) => (
+                      <tr aria-label={release} key={release}>
+                        <td>{release}</td>
+                        <td>
+                          <Field
+                            as={MultiSelect}
+                            items={groupedImages[distro][release]}
+                            name={`${distro}-${release}`}
+                            onItemsUpdate={(items: MultiSelectItem) =>
+                              setFieldValue(getValueKey(distro, release), items)
+                            }
+                            placeholder="Select architectures"
+                            selectedItems={values[getValueKey(distro, release)]}
+                            variant="condensed"
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </span>
+            ))}
+          </Form>
+        )}
+      </FormikForm>
+    </Strip>
   );
 };
 
